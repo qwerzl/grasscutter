adcf5ab3ef05b4f4cbab0de868d9ede4a5af4180
qwerzl <46770502+qwerzl@users.noreply.github.com>
2022-07-22 09:15:21 +0800

    Update action.sh

--- grasscutter/jars/action.sh
+++ grasscutter/jars/action.sh
@@ -1,6 +1,13 @@
 ls Grasscutter
 mv Grasscutter/grasscutter-*.jar grasscutter/jars/
+echo "---------"
+ls Grasscutter
+echo "---------"
 cd ./grasscutter
+ls
+echo "---------"
+ls jars
+echo "----------"
 rm ./grasscutter.jar
 ln -s  ./jars/grasscutter*.jar ./grasscutter.jar
 rm -rf ../Grasscutter
