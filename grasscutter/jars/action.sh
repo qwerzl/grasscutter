1d177cf39b85486d28df87ea9b6c0f38b087644a
qwerzl <46770502+qwerzl@users.noreply.github.com>
2022-07-22 09:51:26 +0800

    Update action.sh

--- grasscutter/jars/action.sh
+++ grasscutter/jars/action.sh
@@ -2,5 +2,5 @@ FILENAME=$(basename Grasscutter/grasscutter-*.jar)
 mv Grasscutter/grasscutter-*.jar grasscutter/jars/
 cd ./grasscutter
 rm ./grasscutter.jar
-ln $(./jars/${FILENAME}) ./grasscutter.jar
+ln ./jars/${FILENAME} ./grasscutter.jar
 rm -rf ../Grasscutter
